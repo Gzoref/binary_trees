3fabdc53148614d02604e11dcb7f79b67427e594
Gzoref <gzoref@gmail.com>
2020-03-09 16:27:53 -0400

    Fix header guard

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
+#ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-#ifdef BINARY_TREES_H
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,16 +19,20 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -50,4 +55,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif // DEBUG
+#endif
