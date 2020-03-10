ac7d8575be7db196c0557489eb29088d3b9b225c
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-09 20:24:26 -0400

    changed 'size' to 'nodes'

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,6 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return (1 + binary_tree_size(tree->left) +
-		binary_tree_size(tree->right));
+	return (1 + binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right));
 }
