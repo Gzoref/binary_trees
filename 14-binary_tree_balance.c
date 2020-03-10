5edd4da019f6fe43325713e6d401242a2b379357
Gzoref <gzoref@gmail.com>
2020-03-10 12:38:50 -0400

    Fixed height and added 14-balance_factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,42 +10,6 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t _left, _right;
-	int balance;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	_left = binary_tree_height(tree->left);
-	_right = binary_tree_height(tree->right);
-
-	if (tree->left != NULL)
-	{
-		_left++;
-	}
-
-	if (tree->right != NULL)
-	{
-		_left++;
-	}
-
-	balance = _left - _right;
-
-	return (balance);
-}
-
-
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree
- * Return: The height of the tree, returns 0 if tree is NULL
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t _left, _right;
 
 	if (tree == NULL)
 	{
@@ -56,10 +20,44 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
 
-	_left = binary_tree_height(tree->left);
-	_right = binary_tree_height(tree->right);
-	if (_left >= _right)
+/**
+ * binary_height_recursion - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: The height of the tree, returns 0 if tree is NULL
+ */
+
+size_t binary_height_recursion(const binary_tree_t *tree)
+{
+	size_t _left, _right;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	_left = binary_height_recursion(tree->left);
+	_right = binary_height_recursion(tree->right);
+
+	if (_left > _right)
+	{
+		return (_left + 1);
+	}
+	else
+	{
 		return (_right + 1);
-	return (_left + 1);
+	}
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: The height of the tree, returns 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (binary_height_recursion(tree) - 1);
 }
