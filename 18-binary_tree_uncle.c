0b6ec54bb65bc3a56be8c06c174125afd28b29d1
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-10 15:38:04 -0400

    fixed docstring

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Finds the sibling of a node
+ * binary_tree_sibling - Finds the sibling of a node
  *
  * @node: Pointer to the node to find the uncle
  *
