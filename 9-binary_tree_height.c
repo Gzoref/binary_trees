62d18d2d5738cd4791c509ca1695ae68e600628a
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 14:32:33 -0400

    even more changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,7 +18,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	_left = binary_tree_height(tree->left);
 	_right = binary_tree_height(tree->right);
+
 	if (_left >= _right)
 		return (_left + 1);
-	return (_right + 1);
+	else
+		return (_right + 1);
 }
