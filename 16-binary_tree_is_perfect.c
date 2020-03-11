6dc82b1844a9dc0645c2e55317366bbf677afddf
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 17:09:44 -0400

    removed a few lines

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,10 +33,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
 	tree_height = binary_tree_height(tree);
 	tree_size = binary_tree_size(tree);
 
