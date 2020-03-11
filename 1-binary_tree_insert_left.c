446e843bc4b747fa357a0d2f6970544451e619ee
Gzoref <gzoref@gmail.com>
2020-03-11 14:03:43 -0400

    fixes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,24 +8,32 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
 
 	if (parent == NULL)
 		return (NULL);
 
-	node = malloc(sizeof(binary_tree_t));
+	node  = malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
 		return (NULL);
 
 	node->n = value;
 	node->parent = parent;
-	node->left = parent->left;
+	node->right = NULL;
 
-	if (parent->left)
+
+	if (parent->left == NULL)
 	{
-		node->left->parent = node;
+		parent->left = node;
+		node->left = NULL;
 	}
-
-	parent->left = node;
+	else
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+		parent->left = node;
+	}
 	return (node);
 }
