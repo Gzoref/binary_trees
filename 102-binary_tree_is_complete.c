8eca094438b78d45145813e952363e830593f231
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 17:34:54 -0400

    added docstrings

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -15,6 +15,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		binary_tree_size(tree->right));
 }
 
+/**
+ * check_if_complete - checks if tree is complete
+ * @tree: Pointer to the root node of the tree
+ * @index: index of where the node is located
+ * @size: size of the tree
+ * Return: 1 if complete, otherwise 0 if not
+ */
+
 int check_if_complete(const binary_tree_t *tree, size_t index, size_t size)
 {
 	if (tree == NULL)
@@ -25,6 +33,11 @@ int check_if_complete(const binary_tree_t *tree, size_t index, size_t size)
 		check_if_complete(tree->right, index * 2 + 2, size));
 }
 
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree
+ * Return: 1 if complete, otherwise 0 if not
+ */
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
