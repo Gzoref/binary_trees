6a204014e786721996c6c2efcfdb2595f36ae8c1
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 15:19:18 -0400

    fixes for 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,10 +25,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		binary_tree_height(tree->right);
 
 	if (balanced == 0)
-	{
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
 
 /**
