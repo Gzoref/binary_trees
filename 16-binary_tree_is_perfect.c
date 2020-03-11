3fb263f337ed07102ca8c89f88a557898c1ae873
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 14:50:19 -0400

    fixed 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,7 +21,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	balanced = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	balanced = binary_tree_height(tree->left) -
+		binary_tree_height(tree->right);
 
 	if (balanced == 0)
 	{
@@ -38,7 +39,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 size_t binary_height_recursion(const binary_tree_t *tree)
 {
-	size_t _left, _right;
+	size_t _left = 0, _right = 0;
 
 	if (tree == NULL)
 	{
