a3743184b26fae34725336a6b6ff26f4af86d382
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-03-11 14:24:30 -0400

    even more changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,29 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf
- *
- * @node: Pointer to the node to check
- *
- * Return: 1 if node, otherwise 0
- */
-
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-	{
-		return (0);
-	}
-
-       /* If next node doesn't exist, is leaf, not parent */
-	if (node->left == NULL && node->right == NULL)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree
  * Return: The height of the tree, returns 0 if tree is NULL
@@ -45,7 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	_left = binary_tree_height(tree->left);
 	_right = binary_tree_height(tree->right);
-	if (_left >= _right)
+	if (_left < _right)
 		return (_right + 1);
 	return (_left + 1);
 }
