8e70799e193b6c5dd10291248ab8681c7e8e5689
Gzoref <gzoref@gmail.com>
2020-03-11 14:08:42 -0400

    fixes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,40 +1,37 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_right - Creates a binary tree node
- *
+ * binary_tree_insert_left - creates a new binary branch on the left
  * @parent: Parent node
- *
  * @value: Value of node
- *
- * Return: Create new Binary tree
+ * Return: Create a new Binary tree
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
+	binary_tree_t *node;
 
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->right = parent->right;
+	if (node == NULL)
+		return (NULL);
 
-	if (parent->right)
+	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+
+
+	if (parent->right == NULL)
 	{
-		new_node->right->parent = new_node;
-
+		parent->right = node;
+		node->right = NULL;
 	}
-	parent->right = new_node;
-	return (new_node);
+	else
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+		parent->right = node;
+	}
+	return (node);
 }
