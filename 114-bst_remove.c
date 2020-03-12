00e82cd6ce560c5c1c47249a823f0318252f97e3
Gzoref <gzoref@gmail.com>
2020-03-12 13:53:42 -0400

    Remove node from BST

---
+++ 114-bst_remove.c
@@ -0,0 +1,76 @@
+#include "binary_trees.h"
+
+/**
+ * recursive_bst_check - Return minmax values
+ *
+ * @tree:  Pointer to the root node of the tree to rotate
+ *
+ *
+ * Return: 1 if BST, 0 if not
+ */
+
+bst_t *min_value(bst_t *node)
+{
+       /* Loop to find leftmost node */
+	bst_t *current = node;
+
+	while (current && current->left != NULL)
+	{
+		current = current->left;
+	}
+	return (current);
+}
+
+/**
+ * bst_remove - Removes a node in a Binary Search Tree
+ *
+ * @root: Pointer to root node of BST
+ *
+ * @value: Value to be found
+ *
+ * Return: A pointer to new root node, or NULL on failure
+ */
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (root == NULL)
+	{
+		return (NULL);
+	}
+
+	/* If node to delete smaller than root value, it is on left */
+	if (value < root->n)
+	{
+		root->left = bst_remove(root->left, value);
+	}
+	/* If node to delete greater than root value, it is on right */
+	else if (value > root->n)
+	{
+		root->right = bst_remove(root->right, value);
+	}
+	/* If value is root's value, delete node */
+	else
+	{
+		/* Node has one or no children */
+		if (root->left == NULL)
+		{
+			/* Remove and return pointer to removed */
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			/* Remove and return pointer to removed */
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+		/* Node with two children */
+		temp = min_value(root->right);
+		root->right = bst_remove(root->right, temp->n);
+	}
+	return (root);
+}
